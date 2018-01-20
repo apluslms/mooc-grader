f864f21571423b1d3e718d66068c8d0c514cd4c6
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2018-01-20 23:12:52 +0200

    Create course static symlinks to STATIC_ROOT
    * Allows relocating statics out of the code directory
      by defining STATIC_ROOT

--- util/static.py
+++ util/static.py
@@ -2,7 +2,7 @@ import os
 from django.conf import settings
 
 def symbolic_link(courses_dir, course):
-    dst = os.path.join(settings.BASE_DIR, 'static', course['key'])
+    dst = os.path.join(settings.STATIC_ROOT, course['key'])
     if not os.path.lexists(dst) and 'static_dir' in course:
         src = os.path.join(courses_dir, course['key'], course['static_dir'])
         os.symlink(src, dst)
