apluslms/mooc-grader

commit 25f3b42fd19511b7bf2e0616009298eacd8cc710

Reload all uwsgi processes

access/config.py
@@ -22,6 +22,7 @@ DIR = os.path.join(settings.BASE_DIR, "courses")
 OLD_DIR = os.path.join(settings.BASE_DIR, "exercises")
 if os.path.isdir(OLD_DIR):
     DIR = OLD_DIR
+DIR = os.path.realpath(DIR) # resolve symlinks
 
 LOGGER = logging.getLogger('main')
 

gitmanager/cron.sh
@@ -2,7 +2,6 @@
 
 FLAG="/tmp/mooc-grader-manager-clean"
 LOG="/tmp/mooc-grader-log"
-TOUCH="/srv/grader/uwsgi-grader.ini"
 SQL="sqlite3 -batch -noheader -column db.sqlite3 "
 TRY_PYTHON="/srv/grader/venv/bin/activate"
 
@@ -12,6 +11,7 @@ if [ -d exercises ]; then
 else
   CDIR=courses
 fi
+CDIR=$(realpath $CDIR)
 USER=$(stat -c '%U' $CDIR/)
 
 if [ -e $FLAG ]; then
@@ -53,6 +53,8 @@ for key in $keys; do
   fi
 done
 
-if [ -e $TOUCH ]; then
-  touch $TOUCH
-fi
+# Reload course configuration by restarting uwsgi processes
+touch $CDIR
+for f in /srv/grader/uwsgi-grader*.ini; do
+    [ -e $f ] && touch $f
+done

commit 6ea5907e66d585142df4ca3e8a3b11ec22cce9e9

Fix _include for absolute paths
This was broken in 25f3b42fd19511b7bf2e0616009298eacd8cc710

access/config.py
@@ -433,6 +433,7 @@ class ConfigParser:
                 # Load new data from rendered include file string
                 render_context = include_data["template_context"]
                 template_name = os.path.join(course_dir, include_file)
+                template_name = template_name[len(DIR)+1:] # FIXME: XXX: NOTE: TODO: Fix this hack
                 rendered = django_template_loader.render_to_string(
                             template_name,
                             render_context