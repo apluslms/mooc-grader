64143fc7591cab2043bd5f1a62e6073d960fb80e
Sampo Kulonen <sampo.kulonen@aalto.fi>
2021-04-28 16:13:12 +0300

    Display hints for correct answers
    Exercises can define hint texts for both correct and incorrect answers.
    Prior to this fix, radio button exercises only displayed hints for
    incorrect answers. This was fixed by removing the extra condition in the
    graded_form_feedback template. Additionally, checkbox exercises only
    displayed hints for options that weren't selected. This was fixed by
    reworking the find_common_hints template tag to include both selected
    unselected options.
    
    Fixes #104

--- access/templatetags/access.py
+++ access/templatetags/access.py
@@ -24,15 +24,21 @@ def find_checkbox_hints(list_of_hints, value):
 def find_common_hints(list_of_hints, checked):
     hints = []
     hints_seen = set()
-    if list_of_hints:
-        if len(checked) == 1 and list_of_hints.get('multiple'):
-            hints.append(list_of_hints.get('multiple'))
 
-        common_hints = list_of_hints.get('not')
-        if common_hints:
-            for hint_value, hint_text in common_hints.items():
-                if not hint_value in checked and hint_value not in hints_seen:
-                    hints_seen.add(hint_value)
-                    hints.append(hint_text)
+    if list_of_hints:
+        for hint_key, hint_value in list_of_hints.items():
+            # 'multiple' and 'not' are special values (see GradedForm.grade_field).
+            if hint_key == 'multiple':
+                if len(checked) == 1:
+                    hints.append(hint_value)
+            elif hint_key == 'not':
+                for not_hint_key, not_hint_value in hint_value.items():
+                    if not_hint_key not in hints_seen and not_hint_key not in checked:
+                        hints_seen.add(not_hint_key)
+                        hints.append(not_hint_value)
+            else:
+                if hint_key not in hints_seen and hint_key in checked:
+                    hints_seen.add(hint_key)
+                    hints.append(hint_value)
 
     return hints
