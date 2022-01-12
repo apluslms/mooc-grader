a0254f3bde4d27d781d190be6e8f852290de9bb5
Tuomas Laine <tuomaslaine97@gmail.com>
2022-01-12 11:20:02 +0000

    Use path() instead of url()
    url() is likely to be deprecated in the future

--- access/urls.py
+++ access/urls.py
@@ -1,19 +1,33 @@
-from django.conf.urls import url
+from django.urls import path, register_converter
 
 from access import views
+from access.converters import BasenameConverter
+
+register_converter(BasenameConverter, "basename")
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^configure$', views.configure, name='configure'),
-    url(r'^test-result$', views.test_result, name='test-result'),
-    url(r'^container-post$', views.container_post, name='container-post'),
-    url(r'^ajax/([\w-]+)/([\w-]+)$', views.exercise_ajax, name='ajax'),
-    url(r'^model/([\w-]+)/([\w-]+)/([\w\d\_\-\.]*)$', views.exercise_model, name='model'),
-    url(r'^exercise_template/([\w-]+)/([\w-]+)/([\w\d\_\-\.]*)$', views.exercise_template, name='exercise_template'),
-    url(r'^generatedfile/([\w-]+)/([\w-]+)/(\d+)/([\w-]+(?:\.\w+)?)$', views.generated_exercise_file,
-        name='generated-file'),
-    url(r'^([\w-]+)/$', views.course, name='course'),
-    url(r'^([\w-]+)/aplus-json$', views.aplus_json, name='aplus-json'),
-    url(r'^([\d-]+|[\w-]+)/([\w-]+)$', views.exercise, name='exercise'),
-    url(r'^login$', views.LoginView.as_view(), name="login"),
+    path("", views.index, name='index'),
+    path("configure", views.configure, name='configure'),
+    path("test-result", views.test_result, name='test-result'),
+    path("container-post", views.container_post, name='container-post'),
+    path("ajax/<slug:course_key>/<slug:exercise_key>", views.exercise_ajax, name='ajax'),
+    path(
+        "model/<slug:course_key>/<slug:exercise_key>/<basename:parameter>",
+        views.exercise_model,
+        name='model',
+    ),
+    path(
+        "exercise_template/<slug:course_key>/<slug:exercise_key>/<basename:parameter>",
+        views.exercise_template,
+        name='exercise_template',
+    ),
+    path(
+        "generatedfile/<slug:course_key>/<slug:exercise_key>/<int:exercise_instance>/<basename:filename>",
+        views.generated_exercise_file,
+        name='generated-file',
+    ),
+    path("<slug:course_key>/", views.course, name='course'),
+    path("<slug:course_key>/aplus-json", views.aplus_json, name='aplus-json'),
+    path("<slug:course_key>/<slug:exercise_key>", views.exercise, name='exercise'),
+    path("login", views.LoginView.as_view(), name="login"),
 ]
