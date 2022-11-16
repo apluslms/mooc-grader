8ff3cd7f5c8b4df5c5057d2bb8ed2fc4ff5ce82c
Markku Riekkinen <markku.riekkinen@aalto.fi>
2022-11-16 19:38:49 +0200

    Add the str method to User for better logging output

--- access/auth.py
+++ access/auth.py
@@ -7,6 +7,12 @@ class User(AnonymousUser):
         self.perms = perms
         self.is_authenticated = True
 
+    def __str__(self) -> str:
+        return str(self.id)
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id}, perms={self.perms})"
+
 
 class Authentication(ServiceAuthentication[AnonymousUser]):
     def get_user(self, request: Request, id: str, payload: dict) -> User:
